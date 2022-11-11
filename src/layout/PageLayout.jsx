c49217eea1174c6089e2fbde6defc448c235634a
Fathi <babayejuadesina@gmail.com>
2022-11-11 17:24:41 +0100

    style: layout maxwidth

--- src/layout/PageLayout.jsx
+++ src/layout/PageLayout.jsx
@@ -3,7 +3,7 @@ import Header from "../component/Header";
 
 const PageLayout = ({ children }) => {
   return (
-    <div>
+    <div className=" max-w-[1440px]">
       <Header />
       <main>{children}</main>
     </div>
