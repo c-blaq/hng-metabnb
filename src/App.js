6ffee65ebd02fc73b5a255094ea0af11af095521
Fathi <babayejuadesina@gmail.com>
2022-11-13 00:31:47 +0100

    feat: page setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
+import PlaceToStay from "./pages/placeToStay";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/place-to-stay" element={<PlaceToStay />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/placeToStay.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PageLayout from "../layout/PageLayout";
+
+const PlaceToStay = () => {
+  return <PageLayout></PageLayout>;
+};
+
+export default PlaceToStay;
