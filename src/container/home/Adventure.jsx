84721be2be406b5a090e4810ef29cf7711b816ee
Fathi <babayejuadesina@gmail.com>
2022-11-12 12:40:40 +0100

    style: responsivenesss

--- src/container/home/Adventure.jsx
+++ src/container/home/Adventure.jsx
@@ -4,11 +4,11 @@ import NftCard from "../../component/NftCard";
 
 const Adventure = () => {
   return (
-    <div className="lg:mt-14 lg:mb-[53px] xl:px-[100px]">
+    <div className="lg:mt-14 lg:mb-[53px]  lg:px-20 xl:px-[100px]">
       <h2 className="font-bold text-center lg:mb-11 lg:text-5xl">
         Inspiration for your next adventure
       </h2>
-      <div className="grid grid-cols-4 gap-6">
+      <div className="grid gap-4 lg:gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {NFTS.slice(0, 8)
           .reverse()
           .map(({ image, title, price, distance, availability }) => (
