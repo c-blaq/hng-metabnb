a5c56e3b14594dc98d26b1e862f90d2ae67ecb81
Fathi <babayejuadesina@gmail.com>
2022-11-13 01:54:59 +0100

    style:responsiveness

--- src/container/placeToStay/Locations.jsx
+++ src/container/placeToStay/Locations.jsx
@@ -8,19 +8,19 @@ const Locations = () => {
     "Castle",
     "fantastic city",
     "beach",
-    "Carcbins",
+    "Carbins",
     "Off-grid",
     "Farm",
   ];
   return (
-    <div className="flex items-center justify-between xl:mt-28 xl:mb-16">
-      <div className="flex gap-12">
+    <div className="text-sm md:text-base flex-wrap text-black xl:text-xl flex items-center justify-between gap-4 md:gap-0 my-7 md:mt-14 xl:mt-28 xl:mb-16">
+      <div className="flex flex-wrap justify-center gap-2 sm:gap-x-4 xl:gap-12">
         {LOCATIONS.map((location) => (
-          <Link className="text-black text-xl">{location}</Link>
+          <Link>{location}</Link>
         ))}
       </div>
 
-      <div className="flex gap-10 xl:mr-12  border border-[#B4B4B4] rounded-2xl py-3 px-4">
+      <div className="flex items-center gap-5 xl:gap-10 xl:mr-12  border border-[#B4B4B4] rounded-lg md:rounded-2xl p-2 md:py-3 md:px-4">
         <span>Location</span>
         <img src="/assets/images/setting.png" alt="setting icon" />
       </div>
