3482c8d76206919003f3296a81187acb5e509b95
Fathi <babayejuadesina@gmail.com>
2022-11-14 20:28:47 +0100

    refactor: image changed

--- src/container/home/AboutMetabnb.jsx
+++ src/container/home/AboutMetabnb.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const AboutMetabnb = () => {
   return (
-    <div className="bg-linear-gradient overflow-hidden gap-4 pb-1 flex flex-row px-4 sm:px-6 lg:px-20 xl:px-[100px] items-center justify-between">
-      <div className="absolute sm:relative z-30 lg:max-w-md">
+    <div className="bg-linear-gradient relative overflow-hidden gap-4 pb-1 flex flex-row px-4 sm:px-6 lg:px-20 xl:px-[100px] items-center justify-between">
+      <div className="absolute px-4 flex flex-col justify-center bg-modalBg top-0 left-0 right-0 bottom-0 sm:bg-inherit sm:block sm:w-1/2 sm:relative z-30 lg:max-w-md">
         <h2 className="font-bold  text-2xl sm:text-3xl lg:text-5xl">
           Metabnb NFTs
         </h2>
@@ -14,28 +14,14 @@ const AboutMetabnb = () => {
         </p>
         <a
           href="/"
-          className="py-2 px-4 rounded-md text-sm sm:text-base sm:py-[14px] sm:px-8 sm:rounded-lg bg-white text-primary"
+          className="py-2 px-4 rounded-md w-1/2 text-center sm:w-auto text-sm sm:text-base sm:py-[14px] sm:px-8 sm:rounded-lg bg-white text-primary"
         >
           Learn more
         </a>
       </div>
 
-      <div className="w-5/6 shrink-0 top-0 mx-auto sm:w-1/2 xl:w-2/3 ">
-        <img
-          src="/assets/images/meta-1.png"
-          alt="metabnb nft"
-          className="relative ml-auto w-60 sm:w-auto rounded-[10px] rotate-[17.6deg] top-16 left-8 md:-left-8 md:top-52 "
-        />
-        <img
-          src="/assets/images/meta-2.png"
-          alt="metabnb nft"
-          className="relative ml-auto w-60 sm:w-auto  rounded-[10px]  -rotate-[26deg] top-0 md:top-32 md:right-72"
-        />
-        <img
-          src="/assets/images/meta-3.png"
-          alt="metabnb nft"
-          className="relative ml-auto w-60 sm:w-auto rounded-[10px] -rotate-[8.76deg] -right-10 bottom-24 md:right-3 md:bottom-20"
-        />
+      <div className="w-full sm:w-1/2 xl:w-auto py-[100px]">
+        <img src="/assets/images/rotated.png" alt="" />
       </div>
     </div>
   );
